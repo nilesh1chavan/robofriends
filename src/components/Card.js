b0624af06b15976513366577613cf0e488fdabea
nilesh_chavan <nilesh.b.chavan@capgemini.com>
2020-04-12 00:25:09 +0800

    Redux changes added

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 const Card = ({id, name, email }) => {
    // const { id, name, email } = props; destructing during function parameters
